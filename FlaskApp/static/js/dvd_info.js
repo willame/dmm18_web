7145b3209ffaeb720a05b4ea8734f9062e1f1877
mengnan wang <wangmn93@gmail.com>
2017-05-08 19:56:07 +0200

    增加页码栏，缩图模式

--- FlaskApp/static/js/dvd_info.js
+++ FlaskApp/static/js/dvd_info.js
@@ -19,12 +19,32 @@ function preview_src(src)
 
 $(document).ready(function() {
 
+
+        // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementById('close-display');
+
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        document.getElementById('display').innerHTML="";
+        modal.style.display = "none";
+
+
+    }
+
+//-----------------------------------------------
     $(".img_s").click(function(){
         var img_s = $(this).attr('src');
         var img_l = preview_src(img_s);
         //$(this).attr('src',img_l);
-        location.hash = "#" + "popup1";
-        $("#display").html("<img width='100%' alt='sample_l' src='"+img_l+"'>")
+//        location.hash = "#" + "popup1";
+//        var re=/^-(\d+)\.[a-z]+/;
+//        match = myRegexp.exec(img_s);
+        modal.style.display = "block";
+        $("#display").html("<img height='80%' alt='sample_l' src='"+img_l+"'>")
 
         $(this).click(function(){
             $(this).attr('src',img_s)
@@ -34,14 +54,14 @@ $(document).ready(function() {
     });
 
     $(".sample_video").click(function(){
-        location.hash = "#" + "popup1";
+        modal.style.display = "block";
         var src = $(this).attr('href');
-
-        $("#display").html("<video width='100%'  autoplay controls><source src='"+
+        $("#display").html("<video width='100%' autoplay controls><source src='"+
         src
         +"'  type='video/mp4'></video>");
-        //$("button").click();
     });
+
+
 
 
 
